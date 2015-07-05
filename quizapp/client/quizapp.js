7a6fa65e257085eace2b4b5d51b20974e7d90d34
nishitsardessai <nishit121@gmail.com>
2015-07-05 19:44:26 +0530

    answer checking, submit button event

--- quizapp/client/quizapp.js
+++ quizapp/client/quizapp.js
@@ -3,10 +3,22 @@ Template.questions.helpers({
 		{ 
 			no: 1,
 			qn: "How are you?",
-			op1: "fine",
-			op2: "fine",
-			op3: "fine",
-			op4: "fine"
+			op1: "fine1",
+			op2: "fine2",
+			op3: "fine3",
+			op4: "fine4",
+			answer: "fine"
+
 	}
 	]
+});
+
+Template.questions.events({
+	'submit .answer': function(event){
+		var answer=event.target.answer.value;
+		if(answer==questions.answer)
+			console.log("correct!");
+		else
+			console.log("wrong");
+	}
 });
