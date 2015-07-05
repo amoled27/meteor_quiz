2a2b697d3fdc955d8b47ff31e0308a532d80c6df
amoled27 <amodshinde1995@gmail.com>
2015-07-06 00:03:16 +0530

    true-false

--- quizapp/client/quizapp.js
+++ quizapp/client/quizapp.js
@@ -1,22 +1,37 @@
 Template.questions.helpers({
-	questions: [
+	  questions : [
 		{ 
 			no: 1,
+			id: 1,
 			qn: "How are you?",
 			op1: "fine1",
 			op2: "fine2",
 			op3: "fine3",
 			op4: "fine4",
-			answer: "fine"
+			ans: "fine2"
+			
 
 	}
 	]
+
+	
+	
+	
 });
+
+var returndata = function(ans_id) {         //function
+		// body...
+		return Questions.findOne({ans:ans_id});
+	}
+
+
 
 Template.questions.events({
 	'submit .answer': function(event){
 		var answer=event.target.answer.value;
-		if(answer==questions.answer)
+		var ans_id = event.target.dataset.trueans; //answer passed is stored in ans_id
+		console.log(ans_id);
+		if(answer==returndata(ans_id)) //checks if ans_id is true
 			console.log("correct!");
 		else
 			console.log("wrong");
