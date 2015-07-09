5e28470c13e60162788e025995de0e8f758ba1d6
nishitsardessai <nishit121@gmail.com>
2015-07-09 19:20:24 +0530

    options wrong selection error solved

--- quizapp/client/quizapp.js
+++ quizapp/client/quizapp.js
@@ -1,6 +1,16 @@
+Template.questions.onRendered(function(){
+  // this.subscribe('allyells');
+  Session.set('skip',0);
+  Session.set('limit',1);
+  //session score, 
+});
+
 Template.questions.helpers({
 	'display': function(){
 		return Questions.find();
+	},
+	'isgt1':function(){
+		return Questions
 	}
 });
 
@@ -9,11 +19,21 @@ Template.questions.events({
 	'submit form': function(event){
 		event.preventDefault();
 		var answer=event.target.choice.value;
-		if(answer==this.answer)
+		if(answer==this.answer){
 			console.log("correct!");
-		else
+		}
+
+		else{
 			console.log("wrong");
-		return false;
+		}
+
+		var limit = Session.get('limit');
+		var skip = Session.get('skip');
+  		Session.set('limit',1+limit);
+  		Session.set('skip',1+skip);
+
+  		return false;
+		
 }
 });
 
