a3bf6eaad85cd066f0c3e5f5261a104ce3716f58
nishitsardessai <nishit121@gmail.com>
2015-07-09 20:13:17 +0530

    10 random questions

--- quizapp/client/quizapp.js
+++ quizapp/client/quizapp.js
@@ -2,13 +2,50 @@ Template.questions.onRendered(function(){
   // this.subscribe('allyells');
   Session.set('skip',0);
   Session.set('limit',1);
-  //session score, 
+  Session.set('score', 0);
+
+  var arr = []
+	while(arr.length < 10){
+  	var randomnumber=Math.ceil(Math.random()*20)
+  	var found=false;
+ 	 for(var i=0;i<arr.length;i++){
+		if(arr[i]==randomnumber){found=true;break}
+ 	 }
+ 	 if(!found)arr[arr.length]=randomnumber;
+	}
+
+
 });
+
+var score=Session.get('score');
 
 Template.questions.helpers({
 	'display': function(){
-		return Questions.find();
+
+
+
+
+
+		var arr = []
+	while(arr.length < 10){
+  	var randomnumber=Math.ceil(Math.random()*20)
+  	var found=false;
+ 	 for(var i=0;i<arr.length;i++){
+		if(arr[i]==randomnumber){found=true;break}
+ 	 }
+ 	 if(!found)arr[arr.length]=randomnumber;
+	}
+
+
+
+
+
+
+		return Questions.find({ no: {$in:arr}});
 	},
+
+
+
 	'isgt1':function(){
 		return Questions
 	}
@@ -21,6 +58,7 @@ Template.questions.events({
 		var answer=event.target.choice.value;
 		if(answer==this.answer){
 			console.log("correct!");
+			score=score+1;
 		}
 
 		else{
