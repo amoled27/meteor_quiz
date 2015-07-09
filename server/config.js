5576874339a19bd8b69d0a1b237f7db0a12f12ed
amoled27 <amodshinde1995@gmail.com>
2015-07-09 12:36:33 +0530

    Randomaization number field added in DB hardcode

--- server/config.js
+++ server/config.js
@@ -18,7 +18,7 @@ Meteor.startup(function() {
   ]
 },
 
-{
+{         "no": 1,
           "question": "Who is the CEO & CTO of spaceX & also the CEO of Tesla Motors",
           "answer": 2,
           "options": [
@@ -28,7 +28,7 @@ Meteor.startup(function() {
             "Sergy Bin"
               ]
 },
-{
+{         "no":2,
           "question": "A vixen is a female of what?",
           "answer": 3,
           "options": [
@@ -39,7 +39,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":3,
           "question": "Who sang 1980's hit song 'Billie Jean'?",
           "answer": 1,
           "options": [
@@ -50,7 +50,7 @@ Meteor.startup(function() {
               ]
 },//-----end
 
-{
+{         "no":4,
           "question": "Havana is the capital city of which country?",
           "answer": 1,
           "options": [
@@ -61,7 +61,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":5,
           "question": "Which wonder of the ancient world was found in Alexandria?",
           "answer": 2,
           "options": [
@@ -71,7 +71,7 @@ Meteor.startup(function() {
             "Temple of Artemis"
               ]
 },//------end
-{
+{         "no":6,
           "question": "Which rapper did Kim Kardashian marry in 2014?",
           "answer": 2,
           "options": [
@@ -81,7 +81,7 @@ Meteor.startup(function() {
             "Dr Dre"
               ]
 },//------end
-{
+{         "no":7,
           "question": "Where did the name Pokemon come from?",
           "answer": 0,
           "options": [
@@ -91,7 +91,7 @@ Meteor.startup(function() {
             "Akira Pokemon"
               ]
 },//------end
-{
+{         "no":8,
           "question": "What was the highest grossing film of 2010?",
           "answer": 2,
           "options": [
@@ -101,7 +101,7 @@ Meteor.startup(function() {
             "Harry Potter"
               ]
 },//------end
-{
+{         "no":9,
           "question": "What is the smallest state in India?",
           "answer": 1,
           "options": [
@@ -111,7 +111,7 @@ Meteor.startup(function() {
             "Mizoram"
               ]
 },//------end
-{
+{         "no":10,
           "question": "Which of the following is the most important cash crop of Assam?",
           "answer": 3,
           "options": [
@@ -122,7 +122,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":11,
           "question": "Which of the following has a potential for harnessing tidal energy in India?",
           "answer": 0,
           "options": [
@@ -136,7 +136,7 @@ Meteor.startup(function() {
 
 
 // -----------------------------------Computer Science-------------------------
-{
+{         "no":12,
           "question": "Who is the creator of Linux?",
           "answer": 0,
           "options": [
@@ -147,7 +147,7 @@ Meteor.startup(function() {
               ]
 },
 
-{
+{         "no":13,
           "question": "Which of these is a media file format that is free from patents?",
           "answer": 1,
           "options": [
@@ -158,7 +158,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":14,
           "question": "'TUX' is the mascot of?",
           "answer": 2,
           "options": [
@@ -168,7 +168,7 @@ Meteor.startup(function() {
             "MIUI"
               ]
 },//------end
-{
+{         "no":15,
           "question": "What notation is used in the discussion  of running time complexity of algorithm?",
           "answer":3 ,
           "options": [
@@ -178,7 +178,7 @@ Meteor.startup(function() {
             "big O notation"
               ]
 },//------end
-{
+{         "no":16,
           "question": "A segment in an IPv6 addresses can be any value from 0 to ___?",
           "answer":3 ,
           "options": [
@@ -188,7 +188,7 @@ Meteor.startup(function() {
             "FFFF"
               ]
 },//------end
-{
+{         "no":17,
           "question": "What does the acronym pixel refer to?",
           "answer": 2,
           "options": [
@@ -199,7 +199,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":18,
           "question": "Which of these algorithm types are used to find quick solution for NP hard problems?",
           "answer": 2,
           "options": [
@@ -210,7 +210,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":19,
           "question": "Which programming language did Alan Kay create?",
           "answer": 2,
           "options": [
@@ -220,7 +220,7 @@ Meteor.startup(function() {
             "smile"
               ]
 },//------end
-{
+{         "no":20,
           "question": "Which NCSA program is considered to be first modern browser?",
           "answer": 3,
           "options": [
@@ -230,7 +230,7 @@ Meteor.startup(function() {
             "Mosaic"
               ]
 },//------end
-{
+{         "no":21,
           "question": "In FORTRAN, what does 'FOR-' stand for?",
           "answer":0 ,
           "options": [
@@ -240,7 +240,7 @@ Meteor.startup(function() {
             "form"
               ]
 },//------end
-{
+{         "no":22,
           "question": "Which Computer connectivity technology is represented by trident logo?",
           "answer": 2,
           "options": [
@@ -250,7 +250,7 @@ Meteor.startup(function() {
             "Bluetooth"
               ]
 },//------end
-{
+{         "no":23,
           "question": "Which of the following use client-server application architecture?",
           "answer": 1,
           "options": [
@@ -260,7 +260,7 @@ Meteor.startup(function() {
             "World Wide Web"
               ]
 },//------end
-{
+{         "no":24,
           "question": "Which of the following is not a JAVA primitive data type?",
           "answer": 3,
           "options": [
@@ -271,7 +271,7 @@ Meteor.startup(function() {
               ]
 },//------end
 // -------------------------------general science-----------------------
-{
+{         "no":25,
           "question": "Lemon contains which kind of acid?",
           "answer": 3,
           "options": [
@@ -282,7 +282,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":26,
           "question": "Who postulated the uncertainty principle?",
           "answer": 2,
           "options": [
@@ -293,7 +293,7 @@ Meteor.startup(function() {
               ]
 },//------end
 
-{
+{         "no":27,
           "question": "In equation E=MC^2 what is C?",
           "answer": 0,
           "options": [
@@ -305,7 +305,7 @@ Meteor.startup(function() {
 },//------end
 
 
-{
+{         "no":28,
           "question": "If G = Conductance , V = voltage & I= Current, which of the following is true?",
           "answer": 1,
           "options": [
