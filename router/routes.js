95563e9a1ef6eee2a6ac12efc7bafeb70172dac6
amoled27 <amodshinde1995@gmail.com>
2015-07-10 08:10:28 +0530

    login|register UI improvements|methods not implemented

--- router/routes.js
+++ router/routes.js
@@ -2,6 +2,59 @@ Router.configure({
   layoutTemplate : 'basiclayout'
 })
 
+
+
+// Router.onBeforeAction(function(){
+// if(!Meteor.userId()){
+//   this.next();
+// }
+// else{
+//   this.redirect('/')
+// }
+
+// },{
+//   only : ['register']
+// });
+
+
+
+// Router.onBeforeAction(function(){
+//   if(Meteor.userId() && Roles.userIsInRole(Meteor.userId(),'admin')){
+//     this.next();
+//   }
+//   else {
+//     this.redirect('/')
+//   }
+
+// },{
+//   only : ['admin']
+// })
+
+
+
+// Router.onBeforeAction(function(){
+
+//   if(!Meteor.userId()){
+//     this.layout('loginlayout');
+//     this.render('login',{to : 'cardcontent'})
+//   }
+//   else {
+//     this.next();
+//   }
+// },{
+//   except : ['about','register','admin']
+// })
+
+
+
+
+
+
+
+
+
+
+
 Router.route('/admin',function(){
 
 	this.render('admin',{to:'maincontent'})
@@ -20,3 +73,14 @@ Router.route('/',function(){
 })
 
 
+Router.route('/register',function(){
+  this.layout('loginlayout');
+  this.render('register',{to : 'cardcontent'});
+})
+
+
+// -------------JUST FOR TESTING--------
+Router.route('/login',function(){
+  this.layout('loginlayout');
+  this.render('login',{to : 'cardcontent'});
+})
