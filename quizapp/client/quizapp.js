14050b44c085781d8a03afa0ccacfbbc73b6c677
nishitsardessai <nishit121@gmail.com>
2015-07-10 12:14:58 +0530

    working shit- till scores(without ui )

--- quizapp/client/quizapp.js
+++ quizapp/client/quizapp.js
@@ -1,19 +1,8 @@
 Template.questions.onRendered(function(){
-  // this.subscribe('allyells');
+  this.subscribe('allyells');
   Session.set('skip',0);
   Session.set('limit',1);
   Session.set('score', 0);
-
-  var arr = []
-	while(arr.length < 10){
-  	var randomnumber=Math.ceil(Math.random()*20)
-  	var found=false;
- 	 for(var i=0;i<arr.length;i++){
-		if(arr[i]==randomnumber){found=true;break}
- 	 }
- 	 if(!found)arr[arr.length]=randomnumber;
-	}
-
 
 });
 
@@ -37,12 +26,13 @@ Template.questions.helpers({
  		 if(!found)arr[arr.length]=randomnumber;
 		}
 		return Questions.find({ no: {$in:arr}});
+		return Questions.find({},{limit:Session.get('limit'),skip:Session.get('skip')});
 	},
 
 
 
-	'isgt1':function(){
-		return Questions
+	'isQuestionCountGT1':function(){
+		return Questions.find().count()>1;
 	}
 });
 
@@ -53,21 +43,33 @@ Template.questions.events({
 		var answer=event.target.choice.value;
 		if(answer==this.answer){
 			console.log("correct!");
+			Materialize.toast('Correct! :-)');
 			var score=Session.get('score');
 			Session.set('score', score+1);
 		}
 
 		else{
 			console.log("wrong");
+			Materialize.toast('Wrong! :-(');
 		}
 
 		var limit = Session.get('limit');
 		var skip = Session.get('skip');
   		Session.set('limit',1+limit);
   		Session.set('skip',1+skip);
-
-  		return false;
 		
-}
+	},
+
+	'click #loadnext':function(event,template){
+		var limit = Session.get('limit');
+		var skip = Session.get('skip');
+  		Session.set('limit',1+limit);
+  		Session.set('skip',1+skip);
+	},
+
+	'click .endQuiz':function(event){}
+
+
+
 });
 
